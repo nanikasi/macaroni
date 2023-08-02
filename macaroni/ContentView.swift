babfeb617bc15c04bc24fa3bfafbcd6640eb130f
Naoto Nakanishi <48846406+nanikasi@users.noreply.github.com>
2023-08-02 13:40:31 +0900

    Initial Commit

---
+++ macaroni/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  macaroni
+//
+//  Created by 中西直人 on 2023/08/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ macaroni/macaroniApp.swift
@@ -0,0 +1,17 @@
+//
+//  macaroniApp.swift
+//  macaroni
+//
+//  Created by 中西直人 on 2023/08/02.
+//
+
+import SwiftUI
+
+@main
+struct macaroniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
